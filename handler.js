d36726f2e64c75619ecf621ecd8d8bc041a6c3e2
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-05-29 23:01:29 -0700

    hussln events

--- handler.js
+++ handler.js
@@ -1,16 +1,66 @@
 'use strict';
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
 
-module.exports.baseball = (event, context, callback) => {
+var fs = require('fs');
+var contents = fs.readFileSync("baseball.json");
+var baseballs = JSON.parse(contents)
+
+module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: 'There is a game today - San Francisco v LA Dodgers at 715',
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.seed = (event, context, callback) => {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  baseballs.forEach(function(data) {
+    var Item = {
+      id: data.id,
+      game: data.opponent,
+      start_time: data.time
+    };
+
+    docClient.put({TableName: 'hustlndb', Item: Item}, (err) => {
+      if (err) {
+        callback(err);
+      }
+      callback(null, {
+        statusCode: 201,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+      });
+    });
+  })
+}
+
+
+module.exports.getEvents = (event, context, callback) => {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  var params = {
+    TableName: 'hustlndb',
+    FilterExpression : 'game = :game_name',
+    ExpressionAttributeValues : {':game_name' : event.query.game}
+  }
+
+  docClient.scan(params, (err, data) => {
+    if (err) {
+      callback(err);
+    }
+
+    var sum = data.Items.reduce((accumulated, current) => {
+      return accumulated + current.rating}
+    , 0);
+
+    var average = sum/data.Items.length;
+
+    callback(null, { averageRating: average });
+  });
+}
